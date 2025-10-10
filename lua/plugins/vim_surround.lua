44ce16bf1025a0db250a237756ea5aa20733bed3
M4tt3 <matteo.bicchi2005@gmail.com>
2025-10-10 13:12:22 +0200

    Removed unnecessary remap for surround

--- lua/plugins/vim_surround.lua
+++ lua/plugins/vim_surround.lua
@@ -4,8 +4,4 @@ return {
     -- runs before plugin is sourced
     vim.g.surround_no_insert_mappings = 1
   end,
-  config = function()
-    -- runs after plugin is loaded
-    vim.keymap.set("i", "<C-s>", "<Plug>Isurround")
-  end,
 }
