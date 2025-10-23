c8135ad62d9113a5b1734e793f225b7f8f1357ce
M4tt3 <matteo.bicchi2005@gmail.com>
2025-10-23 17:13:35 +0200

    Fugitive shortcuts

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,7 +2,18 @@ return {
     "tpope/vim-fugitive",
     config = function() 
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+        vim.keymap.set("n", "<leader>g<Left>", "<cmd>diffget //2<CR>")
+        vim.keymap.set("n", "<leader>g<Right>", "<cmd>diffget //3<CR>")
+        vim.keymap.set("n", "<leader>gc", ':Git commit -m ""<Left>')
+        vim.keymap.set("n", "<leader>gm", "<cmd>Gvdiffsplit!<CR>")
+
+        vim.api.nvim_create_user_command("LocalDiffget", function()
+          vim.cmd("diffget //2")
+        end, {})
+
+        vim.api.nvim_create_user_command("RemoteDiffget", function()
+          vim.cmd("diffget //3")
+        end, {})
+
     end
 }
